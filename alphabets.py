85f0e5877d76081cd761cea1a984745a4dd29869
João Guilherme Storti Scatambuli <jstorti@id.uff.br>
2022-11-26 23:28:39 -0300

    feat: primary_key generate

--- alphabets.py
+++ alphabets.py
@@ -1,15 +1,27 @@
 import string
 from random import shuffle
+from random import choice
 
 class Alphabets:
     
-    alphabet = list(fr'''{string.printable}''')
-    
     def __init__(self):
+        
+        self.alphabet = list(fr'''{string.printable}''')
+
+        self.__alphabet_code = []
+        
+        self.__key_characters = list(fr'''{string.ascii_letters}{string.digits}{string.punctuation}''')
+        
+        for character in self.__key_characters:
+            
+            self.__alphabet_code.append(ord(character))
+
         shuffle(self.alphabet)
-    
-
-
+        
+        self.primary_key = choice(self.__alphabet_code)
+        
 if __name__ == '__main__':
     test = Alphabets()
     print(test.alphabet)
+    print(test.primary_key)
+    print(chr(test.primary_key))
